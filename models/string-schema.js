f58fc575f450b14adfdabdc7517ff63017fdfd22
Vera D <veradanicode@gmail.com>
2025-10-21 16:11:57 +0100

    first commit

---
+++ models/string-schema.js
@@ -0,0 +1,26 @@
+const mongoose =require('mongoose');
+
+const StringSchema=new mongoose.Schema({
+   
+    value:{
+        type:String,
+        required:true
+    },
+    properties:{
+     length:Number,
+    is_palindrome:Boolean,
+    unique_characters:Number,
+    word_count:Number,
+    sha256_hash:String,
+    character_frequency_map:Object,
+   },
+   
+    createdAt:{
+        type:String,
+        default:Date.now()
+    }
+
+
+})
+
+module.exports=mongoose.model('String',StringSchema);
